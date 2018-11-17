9f3300ad38c73bd756d4e25f6185e4bcdade0323
hzy <hzy_uestc@126.com>
2018-11-17 16:29:52 +0800

    提交了Prism框架编写的基本HelloWorld程序

---
+++ HelloWorld/Module.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Module : IModule
+    {
+        private readonly IRegionManager regionManager;
+
+        public Module(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            regionManager.RegisterViewWithRegion("MainRegion", typeof(Views.HelloWorld));
+        }
+    }
+}
---
+++ Prism.Frame/Bootstrapper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Prism.Frame
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return new Shell();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            Application.Current.MainWindow = (Window)this.Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            ModuleCatalog moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
+            moduleCatalog.AddModule(typeof(HelloWorld.Module));
+        }
+    }
+}
